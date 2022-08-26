de2b2ca7cced367f10f232a579398ad9a0530f53
Or Dabool <or.dabool@gmail.com>
2022-08-26 21:27:36 +0300

    Initial Commit

---
+++ react/app/src/App.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h3>iGem React Game</h3>
+			<p>
+				This is where we will develop the iGem game. Already have
+				react-bootstrap and react-icons installed
+			</p>
+		</div>
+	);
+}
+
+export default App;
